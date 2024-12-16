e5258c210f348ec43ac9cf0e34283ddbcbaaed40
Anirudh Singh Naruka <anisingh1304@gmail.com>
2024-12-16 23:38:38 +0530

    Delete GradePrediction directory

--- GradePrediction/GradePrediction/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Import include to reference app URLs
-
-urlpatterns = [
-    path('admin/', admin.site.urls),        # Admin interface
-    path('grades/', include('grades.urls')),  # Include URLs from the grades app
-]
--- GradePrediction/grades/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views  # Import views from the current app
-
-urlpatterns = [
-    path('calculate-required-sgpa/', views.calculate_required_sgpa, name='calculate_required_sgpa'),
-]
